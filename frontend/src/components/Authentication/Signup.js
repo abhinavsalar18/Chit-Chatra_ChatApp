a8d62e830d01c983242b5a7703b7e0934547f7c8
abhinavsalar04 <abhinav.salar99@gmail.com>
2023-06-12 00:03:22 +0530

    Login sign up page added

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,6 +1,8 @@
 import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Show, VStack } from '@chakra-ui/react'
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
+import { useToast } from '@chakra-ui/react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 const Signup = () => {
     const [name, setName] = useState();
     const [email, setEmail] = useState();
@@ -8,15 +10,127 @@ const Signup = () => {
     const [password, setPassword] = useState();
     const [pic, setPic] = useState();
     const [show, setShow] = useState(false);
-
+    const [loading, setLoading] = useState(false);
     const handleClick = () => setShow(!show);
+    const toast = useToast();
+    const history = useHistory();
+
 
     const postDetails = (pics) => {
+        setLoading(true);
+        if(pics === undefined){
+            toast({
+                title: 'Image is invalid!',
+                description: "Please select a valid image!",
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom-center"
+              });
+        }
 
+        // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        // I have made a mistake earlier, I have not copied the full path for image upload
+        // I have juts copied the "https://api.cloudinary.com/v1_1/dosgdhlal"
+        // And as fetch was not able to find the correct end points therefore it was throwing the 
+        // CORS error -> read about it   
+        const apiUrl = 'https://api.cloudinary.com/v1_1/dosgdhlal/image/upload';
+        
+        if (pics.type === 'image/jpeg' || pics.type === 'image/png') {
+          const data = new FormData();
+          data.append('file', pics);
+          data.append('upload_preset', "chat-app");
+          data.append('cloud_name', "dosgdhlal");
+        
+          fetch(apiUrl, {
+            method: 'POST',
+            body: data,
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              setPic(data.url.toString());
+              console.log([{url : data.url.toString()}, {data: data}]);
+              setLoading(false);
+            })
+            .catch((err) => {
+              console.log(err);
+              setLoading(false);
+            });
+        }
+        else{
+            toast({
+                title: 'Image is invalid!',
+                description: "Please select a valid image!",
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: "bottom-center"
+              });
+              setLoading(false);
+              return;
+        }
     };
 
-    const submitHandler = () =>{
+    const submitHandler = async () =>{
+        setLoading(true);
+        if(!name || !email || !password || !confirmPassword){
+            toast({
+                title : "Please fill all the fields!",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom-center'
+            });
+            setLoading(false);
+            return;
+        }
 
+        if(password !== confirmPassword){
+            toast({
+                title : "Passwords do not match!",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom-center'
+            });
+            return;
+        }
+
+        try{
+            // this configuration is for post method while sending data to backend
+            const config = {
+                headers: {
+                    "Content-type" : "application/json",
+                },
+            };
+
+            const { data } = await axios.post(
+                "/api/user",
+                {name, email, password, pic}, 
+                config);
+                toast({
+                    title: "Registration successful!",
+                    status: "success",
+                    duration: 5000,
+                    isClosable: true,
+                    position: "bottom-center"
+                });
+
+                // storing the data into local storage
+                localStorage.setItem("userInfo", JSON.stringify(data));
+                history.push("/chats");
+                setLoading(false);
+        } catch(err){
+            toast({
+                title: "Error occured!",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom-center"
+            });
+            setLoading(false);
+            console.log(err);
+        }
     };
   return (
     <VStack>
@@ -79,6 +193,7 @@ const Signup = () => {
             width="100%"
             style={{marginTop: 15}}
             onClick={submitHandler}
+            isLoading={loading}
         >
             Sign Up
         </Button>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,54 @@
 import React from 'react'
+import { 
+  Container,
+  Box, 
+   Text, 
+   Tab,
+   Tabs, 
+   TabList, 
+   TabPanel,
+   TabPanels 
+  }
+    from "@chakra-ui/react";
+
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+
 
 export const HomePage = () => {
   return (
-    <div>Home</div>
+    <Container maxW="xl" centerContent> 
+      <Box
+       d='flex'
+       justifyContent='center'
+       textAlign='center'
+       p={3}
+       bg={'white'}
+       w='100%'
+       m='40px 0 15px 0'
+       borderRadius='lg'      
+       borderWidth='1px'
+
+      >
+          <Text fontSize='4xl' font-family='work sans'>Chit-Chatra</Text>
+      </Box>
+      <Box bg='white' w='100%' p={4} borderRadius='lg' borderWidth='1px' >
+        <Tabs variant='soft-rounded'>
+        {/* mb -> marginBottom */}
+          <TabList mb='1em'>
+            <Tab width='50%'>Login</Tab>
+            <Tab width='50%'>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+             <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }

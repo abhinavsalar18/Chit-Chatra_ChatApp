ef6fa84f656e21b2c787883e536e6f5745974cd0
abhinavsalar04 <abhinav.salar99@gmail.com>
2023-07-07 10:16:13 +0530

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ root.render(
         <App />
       </ChakraProvider>
     </ChatProvider>
-    </BrowserRouter>
+    </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function

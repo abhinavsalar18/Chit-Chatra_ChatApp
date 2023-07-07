2fab538255d2db2b1173d52cdbdb954beb673ec5
abhinavsalar04 <abhinav.salar99@gmail.com>
2023-07-08 01:00:58 +0530

    real time chattingg implmented using socket.io

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const errorHandler = require('./middlewares/errorHandler');
 const messageRoutes = require("./routes/messageRoutes");
 const cors = require('cors');
+
 dotenv.config();
 
 const connectDB = require("./config/dbConnection");
@@ -36,4 +37,48 @@ app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server is running on port 5000...`.green.bold));
+const server = app.listen(PORT, console.log(`Server is running on port 5000...`.green.bold));
+
+const io = require('socket.io')(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+// for setup
+io.on("connection", (socket) => {
+    // console.log("connected to socket.io");
+
+    socket.on('setup', (userData) => {
+        socket.join(userData._id);
+        console.log("User data:", userData._id);
+        socket.emit('connected');
+    });
+
+// for joining the room
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User joined room: " + room);
+    });
+
+
+//for sending message
+
+    socket.on("new message", (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+
+        if(!chat.users) return console.log(`${chat.users} not defined!`);
+
+        chat.users.forEach(user => {
+            if(user._id == newMessageReceived.sender._id) return;
+
+            socket.in(user._id).emit("message received", newMessageReceived);
+        });
+    });
+
+
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+});
